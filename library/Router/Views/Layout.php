14f18cd8ea17cfe3b137592ced475578a800c4fb
Zeryther <zeryther@gmail.com>
2018-05-21 00:00:15 +0200

    Updated navigation bar

--- library/Router/Views/Layout.php
+++ library/Router/Views/Layout.php
@@ -11,6 +11,7 @@
 
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous"/>
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"/>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -28,29 +29,57 @@
 
 	<body>
 		<nav class="navbar navbar-expand-lg navbar-dark" id="mainHeader">
-			<div class="brand"><a class="navbar-brand" href="#">kostenlos-wlan.in</a></div>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+			<div class="container">
+				<div class="brand"><a class="navbar-brand" href="#">kostenlos-wlan.in</a></div>
 
-			<div class="collapse navbar-collapse" id="navbarSupportedContent">
-				<form class="form-inline" id="search">
-					<div class="form-group" style="display: block;">
-						<label for="searchBox">Ich suche kostenloses WLAN in</label>
-						<input id="searchBox" name="query" class="form-control mr-sm-2" type="search" aria-label="Search">
-					</div>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
 
-					<div class="buttons">
-						<button type="submit" class="btn btn-warning">Suchen</button>
-						
-						<a href="">
-							<button type="button" class="btn btn-warning">Orte mich!</button>
-						</a>
-					</div>
-				</form>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<form class="form-inline mr-auto" id="search">
+						<div class="form-group" style="display: block;">
+							<label for="searchBox">Ich suche kostenloses WLAN in</label>
+							<input id="searchBox" name="query" class="form-control mr-sm-2" type="search" aria-label="Search">
+						</div>
+
+						<div class="buttons">
+							<button type="submit" class="btn btn-warning">Suchen</button>
+							
+							<a href="">
+								<button type="button" class="btn btn-warning">Orte mich!</button>
+							</a>
+						</div>
+					</form>
+
+					<?php
+
+						if(Util::isLoggedIn()){
+							// TODO
+						} else {
+							?>
+					<ul class="navbar-nav">
+						<li class="nav-item dropdown">
+							<a href="#" class="nav-link dropdown-toggle" id="accountDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								<i class="fas fa-user"></i>
+							</a>
+
+							<div class="dropdown-menu dropdown-menu-right" aria-labelledBy="accountDropdown">
+								<a href="<?php print $app->routeUrl("/login"); ?>" class="dropdown-item">Log in</a>
+								<a href="<?php print $app->routeUrl("/register"); ?>" class="dropdown-item">Create account</a>
+							</div>
+						</li>
+					</ul>
+							<?php
+						}
+
+					?>
+				</div>
 			</div>
 		</nav>
 		
-		<?php var_dump($content_for_layout); ?>
+		<div class="container" style="background-color: red">
+			<?= $content_for_layout; ?>
+		</div>
 	</body>
 </html>
