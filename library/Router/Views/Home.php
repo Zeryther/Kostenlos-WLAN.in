8db7196a280a867c20031d0919f367573284a27a
Zeryther <zeryther@gmail.com>
2018-05-21 04:16:50 +0200

    Added login alert

--- library/Router/Views/Home.php
+++ library/Router/Views/Home.php
@@ -1,1 +1,12 @@
+<?php
+
+if(isset($_GET["msg"]) && !empty($_GET["msg"])){
+	$msg = $_GET["msg"];
+
+	if($msg == "loggedIn"){
+		Util::createAlert("loggedIn","Du wurdest erfolgreich eingeloggt.",ALERT_TYPE_SUCCESS,true);
+	}
+}
+
+?>
 alol
