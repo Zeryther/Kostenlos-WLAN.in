370927c57002c2f23479eaa9f8f27ddad628ad15
Zeryther <zeryther@gmail.com>
2018-05-22 21:29:41 +0200

    Fixed Minecraft being required for login

--- library/Router/Routes/LoginRoute.php
+++ library/Router/Routes/LoginRoute.php
@@ -3,7 +3,7 @@
 $app->get("/login", function(){
 	if(!Util::isLoggedIn()){
 		$redir = "http" . (isset($_SERVER["HTTPS"]) ? "s" : "") . "://" . $_SERVER["HTTP_HOST"] . "/loginCallback";
-		$url = "https://gigadrivegroup.com/login?redir=" . urlencode($redir) . "&requiredServices=MINECRAFT&permissions=email,externalAccounts";
+		$url = "https://gigadrivegroup.com/login?redir=" . urlencode($redir) . "&permissions=email";
 
 		$this->reroute($url);
 	} else {
