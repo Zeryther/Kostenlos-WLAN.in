772e46a8943c0a895a38a0675aa4cc941f65c14c
Zeryther <zeryther@gmail.com>
2018-06-26 02:54:49 +0200

    Updated home page headline

--- library/Router/Views/Home.php
+++ library/Router/Views/Home.php
@@ -19,7 +19,7 @@ if(isset($_GET["msg"]) && !empty($_GET["msg"])){
 ?>
 <div class="row">
 	<div class="col-md-8">
-		<h3>Willkommen auf Kostenlos-Wlan.in!</h3>
+		<h3>Willkommen auf Kostenlos-WLAN.in!</h3>
 		<p>
 			Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		</p>
