1999d87ae8ffe1ff1066480bd75944a38ff96777
Zeryther <zeryther@gmail.com>
2018-07-17 19:45:00 +0200

    Updated CacheHandler and Database classes

--- library/Cache/CacheHandler.php
+++ library/Cache/CacheHandler.php
@@ -3,7 +3,22 @@
 use phpFastCache\CacheManager;
 use phpFastCache\Core\phpFastCache;
 
+/**
+ * Class CacheHandler
+ * 
+ * @description Utility methods to use with phpFastCache
+ * @package Cache
+ * @author Gigadrive (support@gigadrivegroup.com)
+ * @copyright 2016-2018 Gigadrive
+ * @link https://gigadrivegroup.com/dev/technologies
+ */
 class CacheHandler {
+	/**
+	 * Gets the phpFastCache CacheManager object
+	 * 
+	 * @access public
+	 * @return CacheManager
+	 */
 	public static function Manager(){
 		static $InstanceCache = null;
 		if($InstanceCache == null){
@@ -13,6 +28,14 @@ class CacheHandler {
 		return $InstanceCache;
 	}
 
+	/**
+	 * Save an object to the cache
+	 * 
+	 * @access public
+	 * @param string $name The key under which the object should be stored (if key is already being used, the old object will be overwritten)
+	 * @param mixed $value The object to be stored
+	 * @param int $expiry The amount in seconds for which the object should be held in the cache
+	 */
 	public static function setToCache($name,$value,$expiry){
 		if(CacheHandler::existsInCache($name)) CacheHandler::deleteFromCache($name);
 		
@@ -23,6 +46,13 @@ class CacheHandler {
 		}
 	}
 	
+	/**
+	 * Get an object from the cache
+	 * 
+	 * @access public
+	 * @param string $name The key under which the object was stored
+	 * @return mixed Returns the cached object
+	 */
 	public static function getFromCache($name){
 		if(CacheHandler::existsInCache($name)){
 			$c = CacheHandler::Manager()->getItem($name);
@@ -33,11 +63,25 @@ class CacheHandler {
 		}
 	}
 	
+	/**
+	 * Returns whether an object exists in the cache
+	 * 
+	 * @access public
+	 * @param string $name The key under which the object was stored
+	 * @return bool Returns true if the object exists
+	 */
 	public static function existsInCache($name){
 		return CacheHandler::Manager()->hasItem($name);
 		//return getFromCache($name) != null;
 	}
 	
+	/**
+	 * Deletes an item from the cache
+	 * 
+	 * @access public
+	 * @param string $name The key under which the object was stored
+	 * @return bool Returns true if the object could be removed
+	 */
 	public static function deleteFromCache($name){
 		$r = false;
 		
--- library/Database/Database.php
+++ library/Database/Database.php
@@ -1,10 +1,39 @@
 <?php
 
+/**
+ * Class Database
+ * 
+ * @description Utility methods to connect to MySQL
+ * @package Database
+ * @author Gigadrive (support@gigadrivegroup.com)
+ * @copyright 2016-2018 Gigadrive
+ * @link https://gigadrivegroup.com/dev/technologies
+ */
 class Database {
+	/**
+	 * @access private
+	 * @var mysqli $db The mysqli object
+	 */
 	private $db = null;
+
+	/**
+	 * @access private
+	 * @var bool $tried Has an attempt been made to connect?
+	 */
 	private $tried = false;
+
+	/**
+	 * @access private
+	 * @var bool $connected Are we connected to the database?
+	 */
 	private $connected = false;
 
+	/**
+	 * Gets the current Database class instance
+	 * 
+	 * @access public
+	 * @return Database
+	 */
 	public static function Instance(){
 		static $inst = null;
 		if($inst == null){
@@ -14,36 +43,63 @@ class Database {
 		return $inst;
 	}
 
+	/**
+	 * Constructor
+	 * 
+	 * @access protected
+	 * @param string $host The mysql host
+	 * @param string $user The mysql user
+	 * @param string $password The mysql password
+	 * @param string $database The main mysql database
+	 * @param int $port The mysql port
+	 */
 	protected function __construct($host,$user,$password,$database,$port = 3306){
 		if($this->tried == false){
 			$this->tried = true;
 			$this->db = @new mysqli($host,$user,$password,$database,$port);
-			$this->db->set_charset("utf8mb4");
 			mysqli_report(MYSQLI_REPORT_ERROR);
 
 			if($this->db->connect_error){
 				die("Connect Error: " . $this->db->connect_error);
 			} else {
+				$this->db->set_charset("utf8mb4");
+
 				$s = $this->db->prepare("SET @@session.time_zone='UTC'");
 				$s->execute();
 				$s->close();
-				
+
 				$connected = true;
 			}
 		}
 	}
 
-	public function __destruct(){
+	/**
+	 * Destructor
+	 * 
+	 * @access protected
+	 */
+	protected function __destruct(){
 		$this->shutdown();
 	}
 
+	/**
+	 * Returns the mysqli object
+	 * 
+	 * @return mysqli
+	 */
 	public function get(){
 		return $this->db;
 	}
 
+	/**
+	 * Returns whether there is currently a connection
+	 * 
+	 * @return bool
+	 */
 	public function connected(){
 		return $this->connected;
 	}
+
 
 	public function fetchAll(){
 		if(func_num_args() >= 1){
@@ -61,6 +117,11 @@ class Database {
 		}
 	}
 
+	/**
+	 * Shuts the mysql connection down
+	 * 
+	 * @return bool Could the connection be shut down?
+	 */
 	public function shutdown(){
 		if($this->connected == true){
 			$this->connected = false;
